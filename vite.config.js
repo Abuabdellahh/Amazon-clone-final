a64741653b18977854f592d86cf336eeefa97ae5
Eibrahim <160432177+Abuabdellahh@users.noreply.github.com>
2024-07-25 18:12:44 +0300

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/Amazon-clone-final/", // replace with your actual repository name
-  build: {
-    chunkSizeWarningLimit: 1000, // Adjust the chunk size limit as needed
-  },
 });
