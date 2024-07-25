24bfc4af22e9e76b9aa6ec21293aaca7fe6b301f
Eibrahim <160432177+Abuabdellahh@users.noreply.github.com>
2024-07-25 17:19:37 +0300

    Deploy Vite app to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/amazon-clone-final/", // replace with your actual repository name
+  base: "/Amazon-clone-final/", // replace with your actual repository name
+  build: {
+    chunkSizeWarningLimit: 1000, // Adjust the chunk size limit as needed
+  },
 });
